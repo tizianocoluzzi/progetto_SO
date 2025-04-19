07ad6736a1648850d4ec602dce6b71aa72d5919d
tiziano <coluzzi.tizi@gmail.com>
2025-04-20 01:09:35 +0200

    inizio lavori
    scrittura del kernel object con il charachter device(da aggiornare e
    migliorare) e praticamente copia del cutecom dalla fase di test

---
+++ src/chardev.c
@@ -0,0 +1,73 @@
+/* file kernel object per la creazione del chardev personalizzato che su azione in scrittura simula pressione dei tasti*/
+#include <linux/module.h> // module init e exit
+#include <linux/init.h> 
+#include <linux/input.h> // per gli input report key e alloc
+#include <linux/input-event-codes.h> // per la lista dei vari tasti
+#include <linux/fs.h> // per le file oparations
+
+static ssize_t device_read(struct file * file, const char __user * buffer, size_t len, loff_t * offset); //taccia il compilatore
+
+static struct input_dev* dev;
+static struct file_operations fops = {.write = device_read };
+
+static ssize_t device_read(struct file * file, const char __user * buffer, size_t len, loff_t * offset){
+    char kbuf[100]; //allochiamo memoria come se non ci fosse un domani
+    int ret = copy_from_user(kbuf, buffer, 1);
+    if(ret < 0) return -EFAULT;
+    kbuf[1] = '\0';
+    printk("ho letto: %s\n", kbuf);
+    return 1; 
+}
+
+static int my_init(void)
+{
+	printk("hello - Hello, Kernel!\n");
+	
+    //crezione di input device 
+    dev = input_allocate_device();
+    if(!dev) printk("errore nell'inizializzazione");
+    
+    // inizializzazione
+    dev->name = "tiziano_dev";
+    dev->id.bustype = BUS_VIRTUAL;
+    dev->id.vendor = 0;
+    dev->id.product = 0;
+    dev->id.version = 0;
+    //in teoria dovrebbe abilitare i bit ad essere inviati
+    dev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_REP);
+    for (int i = 0; i < KEY_CNT; i++)
+        set_bit(i, dev->keybit);
+    
+    //registrazione
+    int error = input_register_device(dev);
+    
+    if(error) printk("errore nella registrazione");
+    
+    // creazione charachter device
+    // lo zero sta ad indicare che assegna lui il major number
+    int major = register_chrdev(0, "tiziano_chardev", &fops);
+    if(major < 0){
+        printk("errore registrando character device \n");
+        input_unregister_device(dev); // la pulizia è necessaria
+    }
+    printk("device registrato con major %d\n", major);
+
+
+    return 0;
+}
+
+static void my_exit(void)
+{
+    input_report_key(dev, KEY_A, 1);
+    input_sync(dev);
+    input_report_key(dev, KEY_A, 0);
+    input_sync(dev);
+    // la free non è necessaria se si fa unregister
+	input_unregister_device(dev);
+    printk("hello - Goodbye, Kernel!\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
---
+++ src/demone.c
@@ -0,0 +1,44 @@
+/* file per la lettura in input del driver /ttyAC0 e la scrittura su un chardev personalizzato creato nel file ./chardev.c*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h> 
+#include <termios.h>
+
+#define PATH "/dev/ttyACM0" 
+
+int main(){
+    //N.B. per aprire il file ho dovuto dare i permessi al device /dev/ttyACM0 il che potrebbe essere un problema per la scalabilità
+    int fd = open(PATH, O_RDONLY | O_NOCTTY);
+    // struttura per la gestione delle opzioni del file
+    struct termios opts;
+
+    if (fd < 0) {
+        perror("error opening file\n");
+        return -1;
+    }
+    
+    opts.c_cflag = B19200 | CS8 | CLOCAL | CREAD; // baudrate, bit per messaggio, le altre boh
+    // le setto a zero per vedere poi che fanno
+    opts.c_iflag = 0;
+    opts.c_oflag = 0;
+    opts.c_lflag = 0;
+
+    // metto i parametri
+    tcflush(fd, TCIFLUSH);
+    tcsetattr(fd, TCSANOW, &opts);
+
+    char buf[1024];
+    memset(buf, 0, 1024);
+    while(1){
+        int ret = read(fd, buf, 1023);
+        if( ret < 0){ 
+            perror("error reading");
+            return -1;
+        }
+        if (ret == 0) continue;
+        printf("line: %s\n", buf);
+    }
+    return 0;
+}
